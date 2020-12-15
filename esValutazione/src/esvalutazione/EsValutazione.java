2b7e56a478403763287886e51984a79345ebf43c
alessandrocicchi <alecch03@gmail.com>
2020-12-15 11:01:52 +0100

    Valutazione

---
+++ esValutazione/src/esvalutazione/EsValutazione.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esvalutazione;
+
+/**
+ *
+ * @author Alessandro
+ */
+public class EsValutazione {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        for(int contatore=0; contatore<10; contatore ++){
+            
+            System.out.println(contatore+1);
+        }
+    }
+    
+}
